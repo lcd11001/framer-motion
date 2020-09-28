d91cd14ba6afafe1a29ccb98ec6a211d5e423065
Luong Cong Dan <lcd11001@gmail.com>
2020-09-28 14:21:58 +0700

    + basic animation

--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Toppings = ({ addTopping, pizza }) => {
   let toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
 
   return (
     <div className="toppings container">
-      
+
       <h3>Step 2: Choose Toppings</h3>
       <ul>
         {toppings.map(topping => {
           let spanClass = pizza.toppings.includes(topping) ? 'active' : '';
           return (
             <li key={topping} onClick={() => addTopping(topping)}>
-              <span className={spanClass}>{ topping }</span>
+              <span className={spanClass}>{topping}</span>
             </li>
           )
         })}
