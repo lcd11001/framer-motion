c30e67aa60d2534e57bf897cb90b5b63f63b0a58
Luong Cong Dan <lcd11001@gmail.com>
2020-09-28 18:09:28 +0700

    + animate exit

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import { motion } from 'framer-motion'
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion'
 import { containerVariants, childVariants } from '../motions'
 
 const Order = ({ pizza }) => {
+  const [showTitle, setShowTitle] = useState(true)
+
+  setTimeout(() => {
+    setShowTitle(false)
+  }, 4000)
+
   return (
     <motion.div className="container order"
       variants={containerVariants}
       initial='hidden'
       animate='visible'
     >
-      <h2>Thank you for your order :)</h2>
+      <AnimatePresence>
+        {
+          showTitle && (
+            <motion.h2
+              exit={{
+                y: '-100vh'
+              }}
+            >
+              Thank you for your order :)
+            </motion.h2>
+          )
+        }
+      </AnimatePresence>
       <motion.p variants={childVariants}>You ordered a {pizza.base} pizza with:</motion.p>
       <motion.div variants={childVariants}>
         {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
